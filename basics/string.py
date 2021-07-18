0fadb827519782757d3bc6b78d8a1a5cce299cbb
haxuyennt38 <haxuyennt38@gmail.com>
2021-07-18 15:49:37 +0200

    add basics part

---
+++ basics/string.py
@@ -0,0 +1,10 @@
+# a = 1
+# b = 2
+# c = a + b
+# print('Hien thi gia tri',c)
+s = 'hello wOrld'
+print(s.capitalize())
+print(s.swapcase())
+print(len(s))
+print(s.lower())
+print(s.replace('wOrld',"every"))
