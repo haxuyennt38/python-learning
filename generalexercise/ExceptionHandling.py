fd7c08ce916029e814c5b5bd18d223c1051f9006
haxuyennt38 <haxuyennt38@gmail.com>
2021-07-19 16:39:00 +0200

    Exception Handling

---
+++ generalexercise/ExceptionHandling.py
@@ -0,0 +1,12 @@
+try :
+    #the first number
+    nb1 = int(input())
+    #the second number
+    nb2 = int(input())
+
+    #calculate total
+    #total = nb1 + nb2
+    #print('the total is equal:',total)
+    print('the total is equal:' + str(nb1 + nb2))
+except :
+    print('exception handling')
