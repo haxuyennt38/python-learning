ff72cc522478611127abada89b9e8ae0ac15cca2
haxuyennt38 <haxuyennt38@gmail.com>
2021-07-27 23:14:51 +0200

    calculate years from year of birth to year today

--- exercices/009.py
+++ exercices/009.py
@@ -1,6 +1,8 @@
+yearOfbirthday = list(range(2021))
+year = int(input())
 count = 0
-yearOfbirthday = int(input())
-for year in range(yearOfbirthday) :
-    print('year')
+for index in range(len(yearOfbirthday)) :
+    print(f'Nam thu : {index}')
     count += 1
 print(count)
+    
