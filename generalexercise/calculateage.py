4980a3b4238c2f70c492535d636fb00f563250c6
haxuyennt38 <haxuyennt38@gmail.com>
2021-07-28 18:53:12 +0200

    Calculateage

---
+++ generalexercise/calculateage.py
@@ -0,0 +1,9 @@
+from datetime import date
+def calculateAge(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year -((today.month, today.day) <(birthDate.month, birthDate.day))
+ 
+    return age
+     
+# Driver code
+print(calculateAge(date(1997, 3, 8)), "years")
