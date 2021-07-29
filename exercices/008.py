1b3e3117730376d13ec290bc19dae26837eda54e
haxuyennt38 <haxuyennt38@gmail.com>
2021-07-29 19:17:32 +0200

    countdown

--- exercices/008.py
+++
@@ -1,7 +0,0 @@
-count = int(input())
-number = count
-for index in range(count) :
-    print(f'Nguoi thu : {index}')
-    count -= 1
-print(count)
-    
---
+++ exercices/08.py
@@ -0,0 +1,8 @@
+numInput = int(input()) 
+for num in range (numInput + 1):
+    print(numInput - num)
+
+
+    
+
+
