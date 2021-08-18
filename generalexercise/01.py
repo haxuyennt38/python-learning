20b8f551b03d32633606a4cd7f3294de40dec0cf
haxuyennt38 <haxuyennt38@gmail.com>
2021-08-18 22:53:19 +0200

    show the octal number octal

---
+++ generalexercise/01.py
@@ -0,0 +1,5 @@
+#taper le système décimal sur le clavier (system decimal)
+systemdecimal = int(input())
+
+#Afficher le système de numération octal (Show the octal number system)
+print('The number decimal %d in the octal number system %o' % (systemdecimal, systemdecimal))
