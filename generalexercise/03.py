075e7f29e0bbdf36b599e55f958808370480c3c9
haxuyennt38 <haxuyennt38@gmail.com>
2021-08-18 23:25:39 +0200

    Show the number decimal after the comma when rounded

---
+++ generalexercise/03.py
@@ -0,0 +1,5 @@
+#Show the number decimal after the comma when rounded
+numberdecimalA = float(input()) #real number type
+numberdecimalB = int(input()) #integer type
+print('{0:.{1}f}'.format(numberdecimalA, numberdecimalB))
+
