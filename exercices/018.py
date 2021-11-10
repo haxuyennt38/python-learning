0a4298de9a84ca9a6e58b539674532ad30208397
haxuyennt38 <haxuyennt38@gmail.com>
2021-11-10 16:32:22 +0100

    name, year old and coparison of the size  between two persons

---
+++ exercices/018.py
@@ -0,0 +1,21 @@
+ten1 = input()
+
+banthu1 = ten1.split(",")
+
+chieucao1 = input(int())
+
+personne1 = "banthu1" + "," + str(chieucao1)
+
+ten2 = input()
+
+banthu2 = ten2.split(",")
+
+chieucao2 = input(int())
+
+personne2 = "banthu2" + "," + str(chieucao1)
+
+if chieucao1 > chieucao2 :
+    print('Ban thu nhat cao hon ban thu 2')
+else :
+    print('Ban thu 2 cao hon ban thu 1')
+
