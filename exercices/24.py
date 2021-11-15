46f02a62e6c69f147afff0d2990b55e95bdcdd0b
haxuyennt38 <haxuyennt38@gmail.com>
2021-11-15 18:05:55 +0100

    calcul the list number - use the loop : for

--- exercices/24.py
+++ exercices/24.py
@@ -9,5 +9,6 @@ if a > b :
     print('it is not the total')
 else :
     for index in range(a, b+1) :
-        total += 1
+        print(index)
+        total += index
     print('it is the total : ', total)
