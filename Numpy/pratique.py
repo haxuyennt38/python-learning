c6a36099799b3612a3be484a72e79cc67421f1b1
haxuyennt38 <haxuyennt38@gmail.com>
2021-12-06 23:59:27 +0100

    pratique

---
+++ Numpy/pratique.py
@@ -0,0 +1,13 @@
+##Créer un vecteur depuis la liste [10, 20, 30] pour assiger le résultat à la variable vector
+
+import numpy as np
+
+vector = np.array([10, 20, 30])
+
+print(vector)
+
+##Créer une matrice depuis la liste suivante :  [[5, 10, 15], [20, 25, 30], [35, 40, 45]]
+
+matrix = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+
+print(matrix)
