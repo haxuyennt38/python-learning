19e47c0b006899d2e2989f695bafdfcc6148cc04
haxuyennt38 <haxuyennt38@gmail.com>
2021-12-07 00:18:13 +0100

    méthode pour la détermination de la taille du tableau

---
+++ Numpy/tailleduntableau.py
@@ -0,0 +1,22 @@
+##Créer un vecteur depuis la liste [10, 20, 30] pour assiger le résultat à la variable vector
+
+#assiger la taille du tableau vector à la variable vector_shape
+
+import numpy as np
+
+vector = np.array([10, 20, 30])
+
+print(vector)
+
+print(vector.shape)
+
+####Créer une matrice depuis la liste suivante :  [[5, 10, 15], [20, 25, 30], [35, 40, 45]]
+
+##assiger la taille du tableau matrix à la variable matrix_shape
+
+matrix = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+
+print(matrix)
+
+print(matrix.shape)
+
